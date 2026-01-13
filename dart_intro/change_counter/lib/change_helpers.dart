0641d65734ad7bb834ded69fa75852eacdc9f875
Tom Maurer <tmaurer@nait.ca>
2026-01-13 15:45:25 -0700

    change counter p1

---
+++ dart_intro/change_counter/lib/change_helpers.dart
@@ -0,0 +1,47 @@
+import 'dart:io';
+
+// Returns an array of coins sorted by face value (p, n, d, q, l, t)
+List<int> promptUserForCoins() {
+  // Initialize variables
+  List<int> output = [];
+
+  var coinTypes = [
+    'pennies',
+    'nickels',
+    'dimes',
+    'quarters',
+    'loonies',
+    'toonies',
+  ];
+
+  // Loop through coin types
+  for (var coinType in coinTypes) {
+    //  Prompt user for coin count
+    stdout.writeln("How many $coinType do you have?");
+    var input = stdin.readLineSync();
+
+    //  Validate input is int
+    late int count;
+
+    try {
+      count = int.parse(input!);
+    } catch (e) {
+      throw Exception('User did not enter a number');
+    }
+
+    //  Validate input is positive
+    if (count < 0) {
+      throw Exception('User entered a negative number');
+    }
+    //  Add to output list
+    output.add(count);
+  }
+
+  // Return output list
+  return output;
+}
+
+// Returns a total value of coins (in cents), given an ordered list of coin counts
+int getTotal(List<int> changeCounts) {
+  return 0;
+}
