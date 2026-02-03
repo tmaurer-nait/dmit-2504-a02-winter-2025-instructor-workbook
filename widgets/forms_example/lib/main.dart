8df388400f8fb893325e55ed9d9c61ea287d82d0
Tom Maurer <tmaurer@nait.ca>
2026-02-03 16:34:18 -0700

    forms pt 4

--- widgets/forms_example/lib/main.dart
+++ widgets/forms_example/lib/main.dart
@@ -42,6 +42,13 @@ class _UserSignupFormState extends State<UserSignupForm> {
   final _passwordController = TextEditingController();
   final _password2Controller = TextEditingController();
 
+  String? _isPasswordValid(String? value) {
+    if (value == null || value.trim().isEmpty) {
+      return 'Password Cannot Be Empty';
+    }
+    return null;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Form(
@@ -57,10 +64,14 @@ class _UserSignupFormState extends State<UserSignupForm> {
           // widget and leverage the form validate/save/reset functionality
           TextFormField(
             controller: _usernameController,
+            validator: (value) => (value == null || value.trim().isEmpty)
+                ? 'Username Cannot be Empty'
+                : null,
             decoration: InputDecoration(label: Text('Username')),
           ),
           TextFormField(
             controller: _passwordController,
+            validator: _isPasswordValid,
             // You should include the following on password fields
             // For user experience reasons
             obscureText: true,
@@ -70,6 +81,15 @@ class _UserSignupFormState extends State<UserSignupForm> {
           ),
           TextFormField(
             controller: _password2Controller,
+            validator: (value) {
+              if (value == null || value.trim().isEmpty) {
+                return 'Password Cannot Be Empty';
+              }
+              if (value != _passwordController.text) {
+                return 'Passwords Do Not Match';
+              }
+              return null;
+            },
             obscureText: true,
             enableSuggestions: false,
             autocorrect: false,
@@ -77,21 +97,32 @@ class _UserSignupFormState extends State<UserSignupForm> {
           ),
           ElevatedButton(
             onPressed: () {
-              // Here is where you handle the submit of the form
-              // Usually you would do some validation and then call an API
-              // update a database or whatever productive thing you want to do
+              // For validation we want to leverage the form.validate
+              // because it calls the validators on every single input
+              // To do so we need to access the form, there are two ways to do that
+              // First: Form.of(context).validate() <- works if the sign up is a
+              // child of the form itself
 
-              // We are just going to show a toast
-              // TODO: Validate
-              ScaffoldMessenger.of(context).showSnackBar(
-                SnackBar(
-                  content: Text(
-                    'Username: ${_usernameController.text}, '
-                    'Password: ${_passwordController.text}, '
-                    'Password 2: ${_password2Controller.text}',
+              // Secondly is to use the key <- works always
+              if (_formKey.currentState!.validate()) {
+                // Here is where you handle the submit of the form
+                // Usually you would do some validation and then call an API
+                // update a database or whatever productive thing you want to do
+
+                // We are just going to show a toast
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(
+                    content: Text(
+                      'Username: ${_usernameController.text}, '
+                      'Password: ${_passwordController.text}, '
+                      'Password 2: ${_password2Controller.text}',
+                    ),
                   ),
-                ),
-              );
+                );
+
+                // To reset the form, access it the same way
+                _formKey.currentState!.reset();
+              }
             },
             child: Text('Sign Up!'),
           ),
