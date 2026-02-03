c9b7958442a63abdd7ba48fd6032710770bb7fa2
Tom Maurer <tmaurer@nait.ca>
2026-02-03 15:47:07 -0700

    forms pt 2

--- widgets/forms_example/lib/main.dart
+++ widgets/forms_example/lib/main.dart
@@ -13,32 +13,47 @@ class MainApp extends StatelessWidget {
       home: Scaffold(
         body: Padding(
           padding: const EdgeInsets.all(16.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                'New User Sign Up',
-                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-              ),
-              TextField(decoration: InputDecoration(label: Text('Username'))),
-              TextField(
-                obscureText: true,
-                enableSuggestions: false,
-                autocorrect: false,
-                decoration: InputDecoration(label: Text('Password')),
-              ),
-              TextField(
-                obscureText: true,
-                enableSuggestions: false,
-                autocorrect: false,
-                decoration: InputDecoration(
-                  label: Text('Confirm Your Password'),
-                ),
-              ),
-              ElevatedButton(onPressed: () {}, child: Text('Sign Up!')),
-            ],
-          ),
+          child: UserSignupForm(),
         ),
+      ),
+    );
+  }
+}
+
+// Extract the widget into its own form widget so we can make it stateful later
+// plus it's cleaner code
+class UserSignupForm extends StatelessWidget {
+  const UserSignupForm({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            'New User Sign Up',
+            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+          ),
+          // Text Form Field rather than TextField to allow connection to the form
+          // widget and leverage the form validate/save/reset functionality
+          TextFormField(decoration: InputDecoration(label: Text('Username'))),
+          TextFormField(
+            // You should include the following on password fields
+            // For user experience reasons
+            obscureText: true,
+            enableSuggestions: false,
+            autocorrect: false,
+            decoration: InputDecoration(label: Text('Password')),
+          ),
+          TextFormField(
+            obscureText: true,
+            enableSuggestions: false,
+            autocorrect: false,
+            decoration: InputDecoration(label: Text('Confirm Your Password')),
+          ),
+          ElevatedButton(onPressed: () {}, child: Text('Sign Up!')),
+        ],
       ),
     );
   }
