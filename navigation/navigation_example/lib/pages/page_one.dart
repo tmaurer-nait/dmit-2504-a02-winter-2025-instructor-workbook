e8c5c2aeaf3182f0e9100d7f78a75601442fda53
Tom Maurer <tmaurer@nait.ca>
2026-02-05 10:48:46 -0700

    routing pt 2

--- navigation/navigation_example/lib/pages/page_one.dart
+++ navigation/navigation_example/lib/pages/page_one.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:navigation_example/pages/page_two.dart';
 
 class PageOne extends StatefulWidget {
   const PageOne({super.key});
@@ -13,9 +14,20 @@ class _PageOneState extends State<PageOne> {
     return Scaffold(
       body: Center(
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Text('PAGE ONE'),
-            ElevatedButton(onPressed: () {}, child: Text('')),
+            ElevatedButton(
+              onPressed: () {
+                // When I press this button I want to go to page two
+                // I'll start with the material page route manual push
+                // (though it's better to use pushNamed with centralized routing)
+                Navigator.of(
+                  context,
+                ).push(MaterialPageRoute(builder: (context) => PageTwo()));
+              },
+              child: Text('Go To Page Two'),
+            ),
             Text('DATA FROM PAGE TWO GOES HERE'),
           ],
         ),
--- navigation/navigation_example/lib/pages/page_two.dart
+++ navigation/navigation_example/lib/pages/page_two.dart
@@ -5,6 +5,16 @@ class PageTwo extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('PAGE TWO'),
+            FilledButton(onPressed: () {}, child: Text('')),
+          ],
+        ),
+      ),
+    );
   }
 }
